cb5705854e2809cfdff324be58e1139d1e2b3ec0
ant9615 <rudals901@gmail.com>
2019-09-10 13:44:03 +0900

    second commit

--- analytics.R
+++ analytics.R
@@ -70,14 +70,31 @@ plot.ts(ages_getjob_old_nopay_ts)
 ###상관분석 
 ##성별에 따른 취업여부
 #피어슨 사용(변수들이 연속형이기에 피어슨을 사용함)
-sex_getjob_corr <- read_xlsx("C:/Users/user/Desktop/sex_getjob.xlsx", sheet = 1)
+sex_getjob_corr <- read_xlsx("C:/Users/user/Desktop/analytics contest/preprocessing data/sex_getjob.xlsx", sheet = 1)
 View(sex_getjob_corr)
 cov(sex_getjob_corr) #분산
 cor(sex_getjob_corr, method = "pearson") #상관관계
 
 ##연령대에 따른 취업여부
 #피어슨 사용(변수들이 연속형이기에 피어슨을 사용함)
-age_getjob_corr <- read_xlsx("C:/Users/user/Desktop/ages_getjob.xlsx", sheet=1)
+age_getjob_corr <- read_xlsx("C:/Users/user/Desktop/analytics contest/preprocessing data/ages_getjob.xlsx", sheet=1)
 View(age_getjob_corr)
 cov(age_getjob_corr)
 cor(age_getjob_corr, method = "pearson")
+
+## 성별과 연령별 상관관계
+#위와 같이 피어슨 사용
+#데이터는 preprocessing한 column data들을 열로 모두 변환하여 정렬한 데이터임
+sex_getjob_corr_1 <- c(10333,	10202,	17597,	17897,	18632,	76839,	44307, 5071,	4995,	2517,	2456,	2567,	94120,	54404, 3541,	3877,	199731,	195102,	195854,	232029,	133439, 738,	838,	103961,	101752,	103904,	224137,	129145)
+age_getjob_corr_1 <- c(3596,	3422,	10934, 10546,	10438,	62247,	37560, 1072,	1219,	99070,	94014,	90947,	275270,	152710, 3909,	4018,	986,	901,	824,	106238,	59641, 428,	504,	78264,	74360,	73323,	4059,	2106)
+cor.test(sex_getjob_corr_1, age_getjob_corr_1, method = "pearson")
+
+###regression 
+#Y=연령 X=성별
+sex_getjob_reg <- c(10333,	10202,	17597,	17897,	18632,	76839,	44307, 5071,	4995,	2517,	2456,	2567,	94120,	54404, 3541,	3877,	199731,	195102,	195854,	232029,	133439, 738,	838,	103961,	101752,	103904,	224137,	129145)
+age_getjob_reg <- c(3596,	3422,	10934, 10546,	10438,	62247,	37560, 1072,	1219,	99070,	94014,	90947,	275270,	152710, 3909,	4018,	986,	901,	824,	106238,	59641, 428,	504,	78264,	74360,	73323,	4059,	2106)
+lm(age_getjob_reg~sex_getjob_reg)
+summary(lm(age_getjob_reg~sex_getjob_reg))
+#Y=성별 X=연령 
+lm(sex_getjob_reg~age_getjob_reg)
+summary(lm(sex_getjob_reg~age_getjob_reg))
